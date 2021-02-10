0d6a5e81a918394f1ae3f629b9d2be4b7b48f1b1
irfan <irfan@jojonomic.com>
2021-02-10 19:39:06 +0700

    update content profesional

--- src/app/services/api/gallery.js
+++ src/app/services/api/gallery.js
@@ -4,7 +4,8 @@ const ENV = process.env.BASE_API
 
 class Gallery {
     static getGallery(lang, page = 1) {
-        let queries = `page=${page}&lang=${lang}`
+        let content_type = sessionStorage.getItem('__STARTER') == 'professional' ? 2 : 1
+        let queries = `page=${page}&lang=${lang}&content_type=${content_type}`
         return axios.get(`${ENV}/api/v1/project-gallery/all?${queries}`)
     }
 
--- src/app/services/api/product.js
+++ src/app/services/api/product.js
@@ -2,8 +2,9 @@ import _DUMMY_PRODUCT from "../_DUMMY/product"
 import axios from 'axios'
 const ENV = process.env.BASE_API
 class Product {
-    static getProduct(page = 1, search = '' , specie = '', construction = 0, rangeWidth = '', rangeThickness = '', rangeLength = '', color = 0) {
-        let queries = `page=${page}&search=${search}&specie=${specie}&construction=${construction}&width=${rangeWidth}&thickness=${rangeThickness}&Length=${rangeLength}&color=${color}`
+    static getProduct(page = 1, search = '', specie = '', construction = 0, rangeWidth = '', rangeThickness = '', rangeLength = '', color = 0) {
+        let content_type = sessionStorage.getItem('__STARTER') == 'professional' ? 2 : 1
+        let queries = `page=${page}&search=${search}&specie=${specie}&construction=${construction}&width=${rangeWidth}&thickness=${rangeThickness}&Length=${rangeLength}&color=${color}&content_type=${content_type}`
         return axios.get(`${ENV}/api/v1/products/all?${queries}`)
     }
 
